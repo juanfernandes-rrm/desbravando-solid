7c19caf702cdcdff37b96005bd833212ba60dcac
Juan Fernandes <juanfernandesrrm@gmail.com>
2024-02-21 15:54:03 -0300

    Add plugin estatisticas-ebook

---
+++ estatisticas-ebook/src/main/java/br/com/cognito/estatisticas/CalculadoraDeEstatisticas.java
@@ -0,0 +1,30 @@
+package br.com.cognito.estatisticas;
+
+import cotuba.domain.Capitulo;
+import cotuba.domain.Ebook;
+import cotuba.plugin.Plugin;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class CalculadoraDeEstatisticas implements Plugin {
+    @Override
+    public String aposRenderizacao(String html) {
+        return html;
+    }
+
+    @Override
+    public void aposGeracao(Ebook ebook) {
+        System.out.println("TESTE");
+        for(Capitulo capitulo : ebook.getCapitulos()) {
+            String html = capitulo.getConteudoHTML();
+            Document document = Jsoup.parse(html);
+            String textoDoCapitulo = document.body().text();
+            String[] palavras = textoDoCapitulo.split("\\s+");
+
+            for(String palavra : palavras) {
+                System.out.println(palavra);
+            }
+
+        }
+    }
+}
