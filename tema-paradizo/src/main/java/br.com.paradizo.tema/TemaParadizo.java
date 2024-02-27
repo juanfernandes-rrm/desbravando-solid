a67dedf2ef7cf26ca5c2198d1920aef42dd05a97
Juan Fernandes <juanfernandesrrm@gmail.com>
2024-02-27 12:05:30 -0300

    Refactor Service Provider to implement AoRenderizarHTML

--- tema-paradizo/src/main/java/br.com.paradizo.tema/TemaParadizo.java
+++ tema-paradizo/src/main/java/br.com.paradizo.tema/TemaParadizo.java
@@ -1,12 +1,11 @@
 package br.com.paradizo.tema;
 
-import cotuba.domain.Ebook;
-import cotuba.plugin.Plugin;
+import cotuba.plugin.AoRenderizarHTML;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 
-public class TemaParadizo implements Plugin {
+public class TemaParadizo implements AoRenderizarHTML {
 
     private String cssDoTema() {
         return FileUtils.getResourceContents("/tema.css");
@@ -27,8 +26,4 @@ public class TemaParadizo implements Plugin {
         return htmlComTema;
     }
 
-    @Override
-    public void aposGeracao(Ebook ebook) {
-
-    }
 }
