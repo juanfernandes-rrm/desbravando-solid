aec89d989ba031b656ff00d35551ca3f1b278ee1
Juan Fernandes <juanfernandesrrm@gmail.com>
2024-03-04 16:01:55 -0300

    Refactor domain model to record

--- cotuba/src/main/java/cotuba/domain/Capitulo.java
+++ cotuba/src/main/java/cotuba/domain/Capitulo.java
@@ -1,23 +1,4 @@
 package cotuba.domain;
-
-public final class Capitulo {
-
-    private final String titulo;
-
-    private final String conteudoHTML;
-
-    public Capitulo(String titulo, String conteudoHTML) {
-        this.titulo = titulo;
-        this.conteudoHTML = conteudoHTML;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-
-    public String getConteudoHTML() {
-        return conteudoHTML;
-    }
-
+public record Capitulo(String capitulo, String conteudoHTML) {
 }
+
