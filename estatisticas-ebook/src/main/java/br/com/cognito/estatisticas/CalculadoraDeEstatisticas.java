df26f9d0423582e987768760eb9ebec49b77f851
Juan Fernandes <juanfernandesrrm@gmail.com>
2024-03-28 11:59:47 -0300

    Add Iterator

--- estatisticas-ebook/src/main/java/br/com/cognito/estatisticas/CalculadoraDeEstatisticas.java
+++ estatisticas-ebook/src/main/java/br/com/cognito/estatisticas/CalculadoraDeEstatisticas.java
@@ -7,15 +7,14 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.text.Normalizer;
-import java.util.Map;
 
 public class CalculadoraDeEstatisticas implements AoFinalizarGeracao {
 
     @Override
     public void aposGeracao(Ebook ebook) {
         var contagemDePalavras = new ContagemDePalavras();
-        for (Capitulo capitulo : ebook.getCapitulos()) {
-            String html = capitulo.getConteudoHTML();
+        for (Capitulo capitulo : ebook.capitulos()) {
+            String html = capitulo.conteudoHTML();
             Document document = Jsoup.parse(html);
             String textoDoCapitulo = document.body().text();
             String textoDoCapituloSemPontuacao = textoDoCapitulo.replaceAll("\\p{Punct}", "");
@@ -27,10 +26,8 @@ public class CalculadoraDeEstatisticas implements AoFinalizarGeracao {
                 contagemDePalavras.adicionarPalavra(emMaiusculas);
             }
 
-            for(Map.Entry<String, Integer> contagem : contagemDePalavras.entrySet()){
-                String palavra = contagem.getKey();
-                Integer ocorrencias = contagem.getValue();
-                System.out.println(palavra + ": " + ocorrencias);
+            for(ContagemDePalavras.Contagem contagem : contagemDePalavras) {
+                System.out.println(contagem.palavra() + " - " + contagem.ocorrencias());
             }
 
         }
