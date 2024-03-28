137d3a33d4149ee7813687f7b3f1852a59832bd7
Juan Fernandes <juanfernandesrrm@gmail.com>
2024-03-28 14:13:41 -0300

    Add cotuba-web module

--- cotuba/cotuba-html/src/main/java/cotuba/html/GeradorHtml.java
+++ cotuba/cotuba-html/src/main/java/cotuba/html/GeradorHtml.java
@@ -1,9 +1,9 @@
 package cotuba.html;
 
-import cotuba.application.GeradorEbook;
 import cotuba.domain.Capitulo;
 import cotuba.domain.Ebook;
 import cotuba.domain.FormatoEbook;
+import cotuba.plugin.GeradorEbook;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -11,49 +11,60 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.Normalizer;
 
-public class GeradorHtml implements GeradorEbook {
+public class GeradorHTML implements GeradorEbook {
 
-    @Override
-    public void gera(Ebook ebook) {
-        Path arquivoDeSaida = ebook.arquivoDeSaida();
-        try{
-            Path diretorioDoHTML = Files.createDirectory(arquivoDeSaida);
+  @Override
+  public FormatoEbook formato() {
+    return FormatoEbook.HTML;
+  }
 
-            int i = 1;
-            for(Capitulo capitulo : ebook.capitulos()) {
-                String nomeDoArquivoHTMLDoCapitulo = obtemNomeDoArquivoHTMLDoCapitulo(i, capitulo);
-                Path arquivoHTMLDoCapitulo = diretorioDoHTML.resolve(nomeDoArquivoHTMLDoCapitulo);
-                String html = """
-                        <!DOCTYPE html>
-                        <html>
-                            <head>
-                                <meta charset="UTF-8">
-                                <title>%s</title>
-                            </head>
-                            <body>
-                                %s
-                            </body>
-                        </html>
-                        """.formatted(capitulo.titulo(), capitulo.conteudoHTML());
-                Files.writeString(arquivoHTMLDoCapitulo, html, StandardCharsets.UTF_8);
-                i++;
-            }
-        } catch (IOException ex) {
-            throw new IllegalStateException("Erro ao criar HTML: " + arquivoDeSaida.toAbsolutePath(), ex);
-        }
+  @Override
+  public void gera(Ebook ebook) {
+
+    Path arquivoDeSaida = ebook.arquivoDeSaida();
+    try {
+
+      Path diretorioDoHTML = Files.createDirectory(arquivoDeSaida);
+
+      int i = 1;
+      for (Capitulo capitulo : ebook.capitulos()) {
+        String nomeDoArquivoHTMLDoCapitulo = obtemNomeDoArquivoHTMLDoCapitulo(i, capitulo);
+
+        Path arquivoHTMLDoCapitulo = diretorioDoHTML.resolve(nomeDoArquivoHTMLDoCapitulo);
+        String html = """
+            <!DOCTYPE html>
+            <html lang="pt-BR">
+              <head>
+                <meta charset="UTF-8">
+                <title>%s</title>
+              </head>
+              <body>
+                %s
+              </body>
+            </html>
+            """.formatted(capitulo.titulo(), capitulo.conteudoHTML());
+        Files.writeString(arquivoHTMLDoCapitulo, html, StandardCharsets.UTF_8);
+
+        i++;
+      }
+
+    } catch (IOException ex) {
+      throw new IllegalStateException("Erro ao criar HTML: " + arquivoDeSaida.toAbsolutePath(), ex);
     }
 
-    @Override
-    public FormatoEbook formato() {
-        return FormatoEbook.HTML;
-    }
+  }
 
-    private String obtemNomeDoArquivoHTMLDoCapitulo(int i, Capitulo capitulo) {
-        String nomeArquivoHTMLCapitulo = i + "-" + removeAcentos(capitulo.titulo().toLowerCase()).replaceAll("[^\\w]", "") + ".html";
-        return nomeArquivoHTMLCapitulo;
-    }
+  private String obtemNomeDoArquivoHTMLDoCapitulo(int i, Capitulo capitulo) {
+    String nomeArquivoHTMLCapitulo = i + "-"
+        + removeAcentos(capitulo.titulo().toLowerCase())
+        .replaceAll("[^\\w]", "")
+        + ".html";
+    return nomeArquivoHTMLCapitulo;
+  }
 
-    private String removeAcentos(String texto) {
-        return Normalizer.normalize(texto, Normalizer.Form.NFC).replaceAll("[^\\p{ASCII}]", "");
-    }
+  private String removeAcentos(String texto) {
+    return Normalizer.normalize(texto, Normalizer.Form.NFD)
+        .replaceAll("[^\\p{ASCII}]", "");
+  }
+
 }
